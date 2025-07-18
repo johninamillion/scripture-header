a869e5be9e2b478b2f76ed717aff60152433e57c
johninamillion <john@millionvisions.de>
2025-07-18 22:55:04 +0200

    🔧 require ScriptureHeaderFixer if not exists

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,6 +4,10 @@ use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 use johninamillion\ScriptureHeader\ScriptureHeaderFixer;
 
+if (!class_exists(ScriptureHeaderFixer::class)) {
+    require_once __DIR__ . '/src/ScriptureHeaderFixer.php';
+}
+
 $finder = Finder::create()
     ->in(__DIR__ . '/src')
     ->name('*.php')
