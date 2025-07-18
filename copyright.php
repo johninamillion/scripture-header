5cf63f6551dda3b1537cc77b93ce9de935aaf9a5
johninamillion <john@millionvisions.de>
2025-07-18 20:31:10 +0200

    🧱 create default copyright template file

---
+++ copyright.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var string $author */
+/** @var string $verse */
+/** @var string $year */
+
+return
+<<<COPYRIGHT
+/**
+* (c) copyright {$year} - {$author}
+* {$verse}
+*/
+COPYRIGHT;
